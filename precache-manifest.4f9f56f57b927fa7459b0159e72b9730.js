9c164a58cec64744b225a821ca5759f84210e229
VinayaBhat <vibha2@pdx.edu>
2020-06-01 14:40:49 -0700

    Updates

---
+++ precache-manifest.4f9f56f57b927fa7459b0159e72b9730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3d29a6190f89c508f4f1f53bc936f3",
+    "url": "/StockDash/index.html"
+  },
+  {
+    "revision": "850e77c2ee93906ffc14",
+    "url": "/StockDash/static/css/2.96745999.chunk.css"
+  },
+  {
+    "revision": "6052c8e6c98ec6d1a569",
+    "url": "/StockDash/static/css/main.0a37f907.chunk.css"
+  },
+  {
+    "revision": "850e77c2ee93906ffc14",
+    "url": "/StockDash/static/js/2.ba10026b.chunk.js"
+  },
+  {
+    "revision": "a4ade2090adca60773209adce09325c2",
+    "url": "/StockDash/static/js/2.ba10026b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6052c8e6c98ec6d1a569",
+    "url": "/StockDash/static/js/main.c067675a.chunk.js"
+  },
+  {
+    "revision": "5df61ea867bf3816aa39",
+    "url": "/StockDash/static/js/runtime-main.a6ea3d88.js"
+  }
+]);
