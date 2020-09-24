a5a36218fcbd92171a9ac869a6c00c33965afa0d
Vinaya Bhat <49253460+VinayaBhat@users.noreply.github.com>
2020-09-24 09:18:21 -0700

    Update RestApiCalls.js

--- src/RestApiCalls.js
+++ src/RestApiCalls.js
@@ -3,7 +3,7 @@
 
 //Load the Latest Quote of the organization data
 export const loadLatestQuote = symbol => {
-  return fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=5min&outputsize=full&apikey=G65K9Q9YE1T9BXFR").then(res => res.json());
+  return fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=5min&outputsize=full&apikey=5YLD33CRQ2OKQ1VW").then(res => res.json());
 };
 
 //Load the company profile data
